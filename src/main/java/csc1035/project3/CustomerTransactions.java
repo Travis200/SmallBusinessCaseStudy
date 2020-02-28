6e927b065a2e7c67c364b3f55c683e7d2f9a3bb5
b9029730 <b9029730@newcastle.ac.uk>
2020-02-28 15:33:03 +0000

    Made a string of items added to the query

--- src/main/java/csc1035/project3/CustomerTransactions.java
+++ src/main/java/csc1035/project3/CustomerTransactions.java
@@ -1,18 +1,26 @@
 package csc1035.project3;
 import java.util.*;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import javax.management.Query;
+
 public class CustomerTransactions {
-    public static void transaction(){
+    public static void transaction() {
+        List<String> items = new ArrayList<>();
+        Session session = HibernateUtil.getSessionFactory().openSession();
         Scanner scanner = new Scanner(System.in);
         String item;
-        do{
+        do {
             System.out.println("What item would you like to add to the transaction or type false to end it");
             item = scanner.nextLine();
             System.out.println("Added item to transaction");
+            items.add(item);
 
         }
-        while(! item.matches("false"));{
+        while (!item.matches("false"));
+        {
 
         }
-
     }
 }
