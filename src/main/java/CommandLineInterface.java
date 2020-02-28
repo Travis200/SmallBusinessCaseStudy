f7820fbac0db28c6fe3944b54b1fc4f7c3d62f39
Tsz Shing Chan <T.Chan3@newcastle.ac.uk>
2020-02-28 14:01:53 +0000

    Stock Count Created. Fix CLI loop.

--- src/main/java/CommandLineInterface.java
+++ src/main/java/CommandLineInterface.java
@@ -1,3 +1,4 @@
+import csc1035.project3.StockCount;
 import java.util.Scanner;
 public class CommandLineInterface {
     public static CommandLineInterface interfaceObj = new CommandLineInterface();
@@ -9,18 +10,21 @@ public class CommandLineInterface {
         System.out.println("Option 3: Update Stock");
         System.out.println("Please input option 1, 2, or 3");
         String userChoice1 = scannerObj.nextLine();
-        while (!(    (userChoice1.equals("1"))||  (userChoice1.equals("2"))  ||  (userChoice1.equals("3"))))   {
+        while (!((userChoice1.equals("1"))||  (userChoice1.equals("2"))  ||  (userChoice1.equals("3"))))   {
             System.out.println("Incorrect input: Please Input 1, 2 or 3");
+            runCLI();
+            break;
         }
         switch(userChoice1){
             case "1":
-                System.out.println("Stock count");
+                System.out.println("Stock count:");
+                StockCount.count();
                 break;
             case "2":
-                System.out.println("Customer Transaction");
+                System.out.println("Customer Transaction:");
                 break;
             case "3":
-                System.out.println("Update stock");
+                System.out.println("Update stock:");
                 break;
             }
         }
---
+++ src/main/java/csc1035/project3/StockCount.java
@@ -0,0 +1,7 @@
+package csc1035.project3;
+
+public class StockCount {
+    public static void count() {
+       System.out.println("The stock count is shown below:");
+    }
+}
