1c67ce8bd569f61afa452b89b14597654a865b5b
B9038013 <b9038013@ncl.ac.uk>
2020-03-04 11:56:33 +0000

    added comments to receipt.java

--- src/main/java/csc1035/project3/Receipt.java
+++ src/main/java/csc1035/project3/Receipt.java
@@ -10,6 +10,7 @@ public class Receipt {
         //Formatting ASCII table
         String leftAlignFormat="|  %-33s    %-4s |%n";
         String leftAlignFormatTotal="|                                       %-4s |%n";
+        //This is how the start of every receipt will look like
         System.out.format("+--------------------------------------------+%n");
         System.out.format("|                 Cash Receipt               | %n");
         System.out.format("+--------------------------------------------+%n");
@@ -21,8 +22,9 @@ public class Receipt {
         System.out.format("|  Item:                              Price: | %n");
         System.out.format("|                                            | %n");
 
-        //Looping through the transaction to print all entries found
+        //Looping through the transaction to print the names and prices of all the items that the customer just bought.
         for(int i=0; i<itemNames.size(); i++){
+            //Prints out the results found in the format of leftAlignFormat so the structure of the ASCII table remains.
             System.out.format(leftAlignFormat,itemNames.get(i),itemPrices.get(i));
         }
 
@@ -33,22 +35,16 @@ public class Receipt {
         int total = 0;
 
         for (int i=0; i<itemPrices.size(); i++){
-
             total += itemPrices.get(i);
         }
+        //After the total cost has been calculated, it prints it out onto the ASCII table in the format of
+        //leftAlignFormatTotal to not break the structure of the table.
         System.out.format(leftAlignFormatTotal, total);
         System.out.format("+--------------------------------------------+%n");
 
 
-
-        /*//Looping through the directory to print all entries found
-                for (Entry entry : directory.toArrayList()) {
-                    //With the alignment specified in String leftAlignFormat, it prints the entries in order of surname,
-                    //initials, and extension.
-                    System.out.format(leftAlignFormat, entry.getSurname(), entry.getInitials(), entry.getTelephoneExtension());
-                }
-                //Closing out the ASCII table at the bottom after all the entries have been printed*/
-        System.out.format("|                                           |%n");
+        //Closing out the ASCII table at the bottom after everything else has been printed
+        System.out.format("|                                            |%n");
         System.out.format("|                  Thank You                 |%n");
         System.out.format("+--------------------------------------------+%n");
 
